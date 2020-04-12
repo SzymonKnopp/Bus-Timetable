64239bc824fbb2404f63f834a1df6760ee761b98
Wisecount <53307980+Wisecount@users.noreply.github.com>
2020-04-12 13:35:14 +0200

    Add files via upload

--- timetable.sh
+++ timetable.sh
@@ -1,44 +1,62 @@
 #!/bin/bash
 
-# DO USUNIĘCIA PÓŹNIEJ!!!
-LINK="https://komunikacja.tczew.pl/chrono.php?stop_id=27&cs=003_20200316-17-2--007_20200316-31-1"
-DAY=Dni # Dni or Soboty or Niedziele
-TIME="1:00"
-AMOUNT=1000
+#DEFAULT VALUES
+LINK=""
+DAY=1
+TIME=$(date +"%H:%M")
+AMOUNT=5
 
 FAVORITES=favorites # path to a file containing saved data
 
+ESCAPE=0
+
 #PROCESS OPTIONS
-ZENITY=0
+ZENITY=1
 while [ -n "$1" ]; do
-    ZENITY=1
+    ZENITY=0
     case "$1" in
-        -p) 
-            echo "Option -p argument: $2"
+        -f) #favorite timetable
+            LINK=$(grep "$2" $FAVORITES | cut -d '#' -f 2)
             shift ;;
-        -c) 
-            echo "Option -c argument: $2"
+
+        -d) #day
+            DAY=$2
             shift ;;
-        
+
+        -t) #time
+            TIME=$2
+            shift ;;
+
+        -c) #count
+            AMOUNT=$2
+            shift ;;
+
+         *) #unknown options
+            echo "Nierozpoznana opcja '$1'"
+            ESCAPE=1
+            shift ;;
     esac
     shift
-
-
-
 done
 
+if [ $ZENITY -eq 0 -a -z "$LINK" ]; then
+    echo "Opcja '-f' jest obowiązkowa"
+    ESCAPE=1
+fi
 
-
-
-ESCAPE=0
 until [ $ESCAPE -eq 1 ]; do
     FAV_LIST=$(cat favorites | cut -d "#" -f 1)
 
-    FUNCTION=$(zenity --entry \
-        --title "Rozkład jazdy" \
-        --text "Co chcesz zrobić?\n\n1. Znajdź autobusy\n2. Dodaj rozkład jazdy\n3. Usuń rozkład jazdy\n4. Zakończ działanie" \
-        --width 200 \
-        2>/dev/null) # redirecting stderr to null to remove warning from terminal
+    if [ $ZENITY -eq 1 ]; then
+        FUNCTION=$(zenity --entry \
+            --title "Rozkład jazdy" \
+            --text "Co chcesz zrobić?\n\n1. Znajdź autobusy\n2. Dodaj rozkład jazdy\n3. Usuń rozkład jazdy\n4. Zakończ działanie" \
+            --width 200 \
+            2>/dev/null) # redirecting stderr to null to remove warning from terminal
+    else
+        ESCAPE=1
+        FUNCTION=1
+    fi
 
     case "$FUNCTION" in
         4) #EXIT
@@ -58,7 +76,7 @@ until [ $ESCAPE -eq 1 ]; do
             zenity --info \
                 --title "Rozkład jazdy" \
                 --text "Pomyślnie dodano do ulubionych" \
-                --width 300
+                --width 300 \
                 2>/dev/null
         ;;
 
@@ -72,46 +90,48 @@ until [ $ESCAPE -eq 1 ]; do
             zenity --info \
                 --title "Rozkład jazdy" \
                 --text "Pomyślnie usunięto z ulubionych" \
-                --width 300
+                --width 300 \
                 2>/dev/null
         ;;
 
 
         1) #GET BUSES
-            zenity --question \
-                --title "Rozkład jazdy" \
-                --text "Czy chcesz wybrać rozkład z ulubionych?" \
-                --width 200 \
-                2>/dev/null
-
-            if [ $? -eq 1 ]; then
-                #MANUAL INPUT
-                LINK=$(zenity --entry \
-                    --title  "Rozkład jazdy" \
-                    --text "Wprowadź link, pod którym znajduje się żądany rozkład jazdy." \
-                    2>/dev/null)
-            else
-                #FROM FAVORITES
-                PICKED=$(zenity --list \
+            if [ $ZENITY -eq 1 ]; then
+                zenity --question \
                     --title "Rozkład jazdy" \
-                    --text "Wybierz rozkład jazdy" \
-                    --column="Nazwa" $FAV_LIST \
-                    2>/dev/null)
-                LINK=$(grep "$PICKED" $FAVORITES | cut -d '#' -f 2)
-            fi
+                    --text "Czy chcesz wybrać rozkład z ulubionych?" \
+                    --width 200 \
+                    2>/dev/null
 
-            #GET ADDITIONAL DATA
-            FORM=$(zenity --forms \
-                --title="Rozkład jazdy" \
-                --text="Wprowadź pozostałe dane" \
-                --separator="#"  \
-                --add-entry="Dzień (1 - Dni powszednie, 2 - Soboty, 3 - Niedziele i święta):" \
-                --add-entry="Godzina (format - HH:MM):" \
-                --add-entry="Liczba autobusów" \
-                2>/dev/null)
-            DAY=$(echo $FORM | cut -d '#' -f 1)
-            TIME=$(echo $FORM | cut -d '#' -f 2)
-            AMOUNT=$(echo $FORM | cut -d '#' -f 3)
+                if [ $? -eq 1 ]; then
+                    #MANUAL INPUT
+                    LINK=$(zenity --entry \
+                        --title  "Rozkład jazdy" \
+                        --text "Wprowadź link, pod którym znajduje się żądany rozkład jazdy." \
+                        2>/dev/null)
+                else
+                    #FROM FAVORITES
+                    PICKED=$(zenity --list \
+                        --title "Rozkład jazdy" \
+                        --text "Wybierz rozkład jazdy" \
+                        --column="Nazwa" $FAV_LIST \
+                        2>/dev/null)
+                    LINK=$(grep "$PICKED" $FAVORITES | cut -d '#' -f 2)
+                fi
+
+                #GET ADDITIONAL DATA
+                FORM=$(zenity --forms \
+                    --title="Rozkład jazdy" \
+                    --text="Wprowadź pozostałe dane" \
+                    --separator="#"  \
+                    --add-entry="Dzień (1 - Dni powszednie, 2 - Soboty, 3 - Niedziele i święta):" \
+                    --add-entry="Godzina (format - HH:MM):" \
+                    --add-entry="Liczba autobusów" \
+                    2>/dev/null)
+                DAY=$(echo $FORM | cut -d '#' -f 1)
+                TIME=$(echo $FORM | cut -d '#' -f 2)
+                AMOUNT=$(echo $FORM | cut -d '#' -f 3)
+            fi
 
             case "$DAY" in
                 1) DAY="Dni powszednie" ;;
@@ -150,10 +170,15 @@ until [ $ESCAPE -eq 1 ]; do
                 # pass on only first records in a specified AMOUNT 
                 head -n $AMOUNT)
 
-            echo "$LIST" | \
-            sed 's# #\t#g' | \
-            zenity --text-info \
-                --title "Rozkład jazdy" \
+            if [ $ZENITY -eq 1 ]; then
+                echo "$LIST" | \
+                sed 's# #\t#g' | \
+                zenity --text-info \
+                    --title "Rozkład jazdy" \
+                    2>/dev/null
+            else
+                echo "$LIST"
+            fi
         ;;
 
         *) ESCAPE=1 ;;
